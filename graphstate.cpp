6822d320c6afb86de5c792a681de4b8f75b95c0c
YoungChul Chun <youngcc3157@gmail.com>
2017-02-16 18:33:47 -0500

    added structure for graphstate.cpp

--- graphstate.cpp
+++ graphstate.cpp
@@ -1,3 +1,59 @@
 #include "graphstate.h"
 
 /* TODO: implement graphstate.cpp */
+
+void GraphState::selectAChild()
+{
+
+}
+void GraphState::selectParent()
+{
+
+}
+void GraphState::selectRoot()
+{
+
+}
+void GraphState::selectLeftSibling()
+{
+
+}
+void GraphState::selectRightSibling()
+{
+
+}
+
+
+/* Add */
+void GraphState::addChildCut()
+{
+
+}
+void GraphState::addChildDoubleCut()
+{
+
+}
+void GraphState::addChildStatement(QString s)
+{
+
+}
+
+/* Remove */
+void GraphState::removeAndSaveOrphans()
+{
+
+}
+void GraphState::removeAndBurnTheOrphanage()
+{
+
+}
+
+/* Surround with cut */
+void GraphState::surroundWithCut()
+{
+
+}
+void GraphState::surroundWithDoubleCut()
+{
+
+}
