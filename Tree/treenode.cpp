0b1ab22709e6f022e9c40c4bed8a71c2aa2b48cb
casey <casey-c@users.noreply.github.com>
2017-02-19 18:22:38 -0500

    More TreeNode and TreeState implementation (still buggy)

--- Tree/treenode.cpp
+++ Tree/treenode.cpp
@@ -92,3 +92,79 @@ TreeNode* TreeNode::addChildPlaceholder()
 
     return newPlaceholder;
 }
+
+/* Add all */
+void TreeNode::addAll(QList<TreeNode *> list)
+{
+    // Check for empty list
+    if (list.isEmpty())
+        return;
+
+    // Check if this is allowed to have children
+    if (isStatement())
+        return;
+
+    // This node is a placeholder
+    if (isPlaceHolder())
+    {
+        TreeNode* first = list.first();
+        list.removeFirst();
+
+        // Replace this with first node in the list
+        this->type = first->type;
+        this->name = first->name;
+
+        // Add the rest as siblings to this node
+        for (TreeNode* node : list)
+            parent->addExisting(node);
+
+        // Finished
+        return;
+    }
+
+    // This is a parent of a placeholder
+    else if (placeHolderChild)
+    {
+        // Replace the placeholder child with the first item in the list
+        TreeNode* first = list.first();
+        list.removeFirst();
+
+        placeholder->type = first->type;
+        placeholder->name = first->name;
+
+        placeHolderChild = false;
+
+        // Then add the other items as additional children
+        for (TreeNode* node : list)
+            this->addExisting(node);
+    }
+
+}
+
+
+/* Add an existing node as a child */
+void TreeNode::addExisting(TreeNode *node)
+{
+    // Check to make sure the existing node doesn't conflict with placeholder
+    // restrictions
+    if (node->isPlaceHolder() && this->placeHolderChild)
+        return;
+
+    // Otherwise, we should be free to add a new child
+    TreeNode* newNode = new TreeNode(node->type,this,node->name);
+    children.append(newNode);
+}
+
+/*
+ * Remove this node from the tree
+ *
+ * Warning: removing a node will DELETE it from the heap
+ */
+void TreeNode::remove()
+{
+    // Remove us from our parent's list of children
+    parent->children.removeOne(this);
+
+    // Delete us permanently
+    delete this;
+}
