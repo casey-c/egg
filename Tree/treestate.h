91ef34df6476a5c9de377207f569347e6c9b33a1
casey <casey-c@users.noreply.github.com>
2017-03-07 07:38:18 -0500

    Documentation updates (WIP)

--- Tree/treestate.h
+++ Tree/treestate.h
@@ -11,24 +11,61 @@
  * region and handling a modify command), which will handle any substantial
  * changes within the graph.
  *
- * The constructor makes a new root node and selects it.
+ * The constructor makes a new root node and highlights it.
  *
- * Change selection will update the selected tree node (which will serve as
- * the source of modifications, additions, and removals).
+ * There are a couple of concepts to understand before working with a TreeState:
  *
- * Additions will try and add a child of a specific type to the selected
- * region. If the selected region is a statement (which is not allowed to have
- * children of its own), then the addition will add it as a child of the
- * selected region's parent (i.e. make it a new sibling to the selected
- * statement).
+ * Highlight vs. Selection:
+ *      highlight: a single pointer indicating what the mouse is on / where the
+ *                 keyboard is, where the focus should be
+ *      selection: a collection of sibling nodes that act as an extension of the
+ *                 highlighted node; this enables operations to act on multiple
+ *                 nodes at once, instead of just the single highlighted node
  *
- * Removals come in two flavors: saveOrphans will transfer all of the selected
- * region's children to their grandparent; burnTheOrphanage will remove and kill
- * all of the selected region's children if they have any. In either case, the
- * selected region will be removed from the tree. Root elements are not allowed
- * to be removed.
+ *    * Both highlight and selection act similar; if nothing is selected, the
+ *      operations will all act on the highlighted node instead. Selections just
+ *      allow for multi-selection to repeat operations on many nodes. They are
+ *      also critical to the design of iterations and deiterations, which act
+ *      upon subgraphs as points of copy and erasure.
  *
- * Surround with cut simply adds a cut above the selected region in the tree.
+ * Basic operations (EDIT mode):
+ *      add:       builds the tree by adding nodes beneath the highlighted node
+ *                 or the selected nodes
+ *      highlight: moves the highlighted node, effectively handling a mouse move
+ *                 or a keyboard movement key press
+ *      select:    changes the selection list; makes sure we don't violate any
+ *                 of the restrictions imposed upon selection
+ *      remove:    permanently deleting a node from the tree - this is
+ *                 unrecoverable, and is used after undoing an add call
+ *      detach:    similar to remove, however the node isn't actually deleted,
+ *                 a pointer to the detached node is stored in a command
+ *                 somewhere. this is what is called through the detach commands
+ *                 (N key, M key) so that the processes are easily undoable and
+ *                 nodes don't have to be deleted and remade often; just detach
+ *                 and move back into place
+ *      move:      essentially changes the parent of a treenode, making it move
+ *                 on the tree structure. there's some logic to ensure the tree
+ *                 integrity isn't violated (so can't move a root down the tree
+ *                 or anything silly like that)
+ *      surround:  for surrounding a selection with a cut or double cut
+ *
+ * Modification operations (MODIFICATION mode):
+ *      DC:        surround anything with a double cut, remove any double cut
+ *                 that surrounds a node (if the double cut is contiguous)
+ *      Insertion: enter EDIT mode on any odd level subgraph
+ *      Erasure:   detach anything on an even level subgraph
+ *      It/Deit:   copies the selectionList's subgraph into a valid new location
+ *                 or finds duplicates of it and removes them. these have some
+ *                 important restrictions too.
+ *
+ * Drawing:
+ *      boxprint:  draws the tree inside a box, using a recursive call to the
+ *                 corresponding drawing functions of tree nodes
+ *      pounce:    very similar to boxprint, except the node info themselves are
+ *                 replaced with the text strings that can be typed to pounce
+ *                 there (i.e. rapid movement for highlight through the
+ *                 keyboard alone) - based on vim plugin EasyMotion and chrome
+ *                 plugin vimium's link clicking functionality (F key)
  */
 class TreeState : public QObject
 {
